af317e4bdbfeb2a19f570a54dc23ab0e9b83ad6c
Vu Anh LE <anhlv2312@gmail.com>
2018-10-24 23:40:36 +1000

    Uses of Simulator to generate two states and insert into graph

--- src/botmate/Solver.java
+++ src/botmate/Solver.java
@@ -1,30 +1,68 @@
 package botmate;
 
-import problem.ProblemSpec;
-import problem.TirePressure;
+import botmate.adts.*;
+import problem.*;
+import simulator.*;
+
 
 import java.io.IOException;
 
 public class Solver {
     public static void main(String[] args) {
-        ProblemSpec ps;
         try {
-            ps = new ProblemSpec("botmate.input1.txt");
-            solve(ps);
+            ProblemSpec ps = new ProblemSpec("botmate.input1.txt");
+            Simulator sim = new Simulator(ps);
+            solve(ps, sim);
         } catch (IOException e) {
             System.out.println("IO Exception occurred");
             System.exit(1);
         }
-        System.out.println("Finished loading!");
+    }
+
+    private static void solve(ProblemSpec ps, Simulator sim) {
+        // initialize a graph (true: directed)
+        Graph<State, Action> states = new AdjacencyMapGraph<>(true);
+        State initialState, nextState;
+
+        // Generate the initial state
+        initialState = State.getStartState(ps.getFirstCarType(), ps.getFirstDriver(), ps.getFirstTireModel());
+        System.out.println(initialState + ": " + toString(initialState));
+
+        // Insert the initial state into the graph
+        Vertex<State> initialVertex = states.insertVertex(initialState);
+
+        // Initialize a sample action
+        Action firstAction = new Action(ActionType.CHANGE_CAR, "toyota");
+
+        // Simulate the action and get the next state
+        nextState = sim.step(firstAction);
+
+        // Insert the next state into the graph
+        Vertex<State> nextVertex = states.insertVertex(nextState);
+
+        // Insert the edge (of action) from initial to next state,
+        // the edge could be Action or an object of action and probability
+        states.insertEdge(initialVertex, nextVertex, firstAction);
+        System.out.println(nextState + ": " +  toString(nextState));
+
+        System.out.println(states);
 
     }
 
-    private static void solve(ProblemSpec ps) {
-        State initialState;
-        initialState = new State(1, false, 0, false, 0, ps.getFirstCarType(), ProblemSpec.FUEL_MAX,
-                TirePressure.ONE_HUNDRED_PERCENT, ps.getFirstDriver(), ps.getFirstTireModel());
-        System.out.println(initialState.toString());
 
+    private static String toString(State state) {
+        String output = "";
+        output += ("(");
+        output += (state.getPos() + ",");
+        output += (state.isInSlipCondition()?1:0 + ",");
+        output += (state.isInBreakdownCondition()?1:0 + ",");
+        output += (state.getCarType() + ",");
+        output += (state.getDriver() + ",");
+        output += (state.getTireModel() + ",");
+        output += (state.getFuel() + ",");
+        output += (state.getTirePressure());
+        output += (")");
+        return output;
     }
 
 }
